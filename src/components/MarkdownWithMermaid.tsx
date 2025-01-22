b258359b49e0b3d1f7f3e85ac5db796e79fcf412
seoeun Park <sseonxx@gmail.com>
2025-01-22 14:41:31 +0900

    렌더링 이슈 수정

--- src/components/MarkdownWithMermaid.tsx
+++ src/components/MarkdownWithMermaid.tsx
@@ -1,20 +1,23 @@
-import mermaid from 'mermaid'
-import React, { useEffect } from 'react'
+import mermaid from 'mermaid';
+import React, { useEffect, useRef } from 'react';
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialOceanic } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 type MarkdownWithMermaidProps = {
-  content: string
-}
+  content: string;
+};
 
 const MarkdownWithMermaid: React.FC<MarkdownWithMermaidProps> = ({ content }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
-    // Mermaid 초기화
-    mermaid.initialize({ startOnLoad: true });
+    mermaid.initialize({ startOnLoad: false });
 
     const renderMermaidCharts = async () => {
-      const mermaidElements = document.querySelectorAll(".mermaid");
+      if (!containerRef.current) return;
+
+      const mermaidElements = containerRef.current.querySelectorAll(".mermaid");
       for (let index = 0; index < mermaidElements.length; index++) {
         const el = mermaidElements[index];
         const mermaidCode = el.textContent || "";
@@ -31,58 +34,58 @@ const MarkdownWithMermaid: React.FC<MarkdownWithMermaidProps> = ({ content }) =>
     renderMermaidCharts();
   }, [content]);
 
-
   return (
-    <ReactMarkdown
-      components={{
-        code: ({ node, inline, className, children, ...props }) => {
-          const match = /language-(\w+)/.exec(className || "");
+    <div ref={containerRef}>
+      <ReactMarkdown
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || "");
 
-          // Mermaid 처리
-          if (match && match[1] === "mermaid") {
-            return (
-              <div className="mermaid">
+            // Mermaid 처리
+            if (match && match[1] === "mermaid") {
+              return (
+                <div className="mermaid">
+                  {String(children).replace(/\n$/, "")}
+                </div>
+              );
+            }
+
+            // 일반 코드 블록 처리
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={materialOceanic}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
                 {String(children).replace(/\n$/, "")}
-              </div>
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
             );
-          }
-
-          // 일반 코드 블록 처리
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={materialOceanic}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            >
-              {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-        img: ({ src, alt }) => (
-          <div style={{ textAlign: 'center', marginTop: '10px' }}>
-            <img
-              src={src}
-              alt={alt}
-              style={{
-                maxWidth: '100%',
-                height: 'auto',
-                display: 'block',
-                margin: '0 auto',
-              }}
-            />
-          </div>
-        ),
-
-      }}
-    >
-      {content}
-    </ReactMarkdown>
+          },
+          img: ({ src, alt }) => (
+            <div style={{ textAlign: "center", marginTop: "10px" }}>
+              <img
+                src={src}
+                alt={alt}
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                  display: "block",
+                  margin: "0 auto",
+                }}
+              />
+            </div>
+          ),
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
   );
-}
+};
 
-export default MarkdownWithMermaid
+export default MarkdownWithMermaid;
